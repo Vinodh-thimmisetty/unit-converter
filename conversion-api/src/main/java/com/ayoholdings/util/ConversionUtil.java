339c527889de671fe71084275d9f102ec6be81a0
Vinodh Kumar Thimmisetty <vinodh5052@gmail.com>
2021-02-08 18:54:57 +0530

    Sonar Issue fixes

--- conversion-api/src/main/java/com/ayoholdings/util/ConversionUtil.java
+++ conversion-api/src/main/java/com/ayoholdings/util/ConversionUtil.java
@@ -48,17 +48,17 @@ public class ConversionUtil {
                 .name("Ounce")
                 .label("ounce").build();
 
-        UnaryOperator<Double> kgToPound = (k) -> (k * 2.20_462);
-        UnaryOperator<Double> kgToOunce = (k) -> (k * 35.274);
+        UnaryOperator<Double> kgToPound = k -> (k * 2.20_462);
+        UnaryOperator<Double> kgToOunce = k -> (k * 35.274);
 
-        UnaryOperator<Double> tonneToPound = (k) -> (k * 2_204.62);
-        UnaryOperator<Double> tonneToOunce = (k) -> (k * 35274);
+        UnaryOperator<Double> tonneToPound = k -> (k * 2_204.62);
+        UnaryOperator<Double> tonneToOunce = k -> (k * 35274);
 
-        UnaryOperator<Double> poundToKg = (p) -> (p * 0.453_592);
-        UnaryOperator<Double> ounceToKg = (o) -> (o * 0.0_283_495);
+        UnaryOperator<Double> poundToKg = p -> (p * 0.453_592);
+        UnaryOperator<Double> ounceToKg = o -> (o * 0.0_283_495);
 
-        UnaryOperator<Double> poundToTonne = (p) -> (p * 0.000_453_592);
-        UnaryOperator<Double> ounceToTonne = (o) -> (o * 0.00_002_835);
+        UnaryOperator<Double> poundToTonne = p -> (p * 0.000_453_592);
+        UnaryOperator<Double> ounceToTonne = o -> (o * 0.00_002_835);
 
         List<ConversionFactor> conversions = new ArrayList<>();
         conversions.add(ConversionFactor.builder().from(kg).to(pound).converter(kgToPound).build());
@@ -88,14 +88,14 @@ public class ConversionUtil {
                 .name("Kelvin")
                 .label("k").build();
 
-        UnaryOperator<Double> celToFah = (cel) -> (cel * 1.8) + 32;
-        UnaryOperator<Double> celToKel = (cel) -> cel + 273.15;
+        UnaryOperator<Double> celToFah = cel -> (cel * 1.8) + 32;
+        UnaryOperator<Double> celToKel = cel -> cel + 273.15;
 
-        UnaryOperator<Double> fahToCel = (fah) -> (fah - 32) * 0.5555;
-        UnaryOperator<Double> fahToKel = (fah) -> (fah - 32) * 0.5555 + 273.15;
+        UnaryOperator<Double> fahToCel = fah -> (fah - 32) * 0.5555;
+        UnaryOperator<Double> fahToKel = fah -> (fah - 32) * 0.5555 + 273.15;
 
-        UnaryOperator<Double> kelToCel = (kel) -> kel - 273.15;
-        UnaryOperator<Double> kelToFah = (kel) -> (kel - 273.15) * 1.8 + 32;
+        UnaryOperator<Double> kelToCel = kel -> kel - 273.15;
+        UnaryOperator<Double> kelToFah = kel -> (kel - 273.15) * 1.8 + 32;
 
         List<ConversionFactor> conversions = new ArrayList<>();
         conversions.add(ConversionFactor.builder().from(celsius).to(fahrenheit).converter(celToFah).build());
@@ -129,22 +129,22 @@ public class ConversionUtil {
                 .name("Foot")
                 .label("foot").build();
 
-        UnaryOperator<Double> kmToInch = (km) -> km * 39_370.1;
-        UnaryOperator<Double> kmToYard = (km) -> km * 1_093.61;
-        UnaryOperator<Double> kmToFoot = (km) -> km * 3280.84;
+        UnaryOperator<Double> kmToInch = km -> km * 39_370.1;
+        UnaryOperator<Double> kmToYard = km -> km * 1_093.61;
+        UnaryOperator<Double> kmToFoot = km -> km * 3280.84;
 
-        UnaryOperator<Double> mToInch = (m) -> m * 39.3_701;
-        UnaryOperator<Double> mToYard = (m) -> m * 1.09_361;
-        UnaryOperator<Double> mToFoot = (m) -> m * 3.28_084;
+        UnaryOperator<Double> mToInch = m -> m * 39.3_701;
+        UnaryOperator<Double> mToYard = m -> m * 1.09_361;
+        UnaryOperator<Double> mToFoot = m -> m * 3.28_084;
 
-        UnaryOperator<Double> inchToKm = (i) -> i * 0.0_000_254;
-        UnaryOperator<Double> inchToM = (i) -> i * 0.0_254;
+        UnaryOperator<Double> inchToKm = i -> i * 0.0_000_254;
+        UnaryOperator<Double> inchToM = i -> i * 0.0_254;
 
-        UnaryOperator<Double> yardToKm = (y) -> y * 0.0_009_144;
-        UnaryOperator<Double> yardToM = (y) -> y * 0.9_144;
+        UnaryOperator<Double> yardToKm = y -> y * 0.0_009_144;
+        UnaryOperator<Double> yardToM = y -> y * 0.9_144;
 
-        UnaryOperator<Double> footToKm = (f) -> f * 0.0_003_048;
-        UnaryOperator<Double> footToM = (f) -> f * 0.3048;
+        UnaryOperator<Double> footToKm = f -> f * 0.0_003_048;
+        UnaryOperator<Double> footToM = f -> f * 0.3048;
 
         List<ConversionFactor> conversions = new ArrayList<>();
         conversions.add(ConversionFactor.builder().from(kilometer).to(inch).converter(kmToInch).build());
